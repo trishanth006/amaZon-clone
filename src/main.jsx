d349e1dd16fc7ff2550c9c04860b04f3a9a1bfb9
trishanth006 <trishanth6666@gmail.com>
2024-09-24 22:45:44 +0530

    home-changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Nav from './nav.jsx'
+import Home from './Home.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Nav/>
+    <Home/>
   </StrictMode>,
 )
